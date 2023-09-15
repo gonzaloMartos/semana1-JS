ed39a192196af5005f630eab4cb6da299c9b7dbd
gonzaloMartos <gonzalomartos2002@gmail.com>
2023-09-14 21:03:14 -0300

    semana1-JS

---
+++ app.js
@@ -0,0 +1,72 @@
+//Sumar
+function sumar(x, y){
+    return x + y;
+}
+
+sumar(2, 4);
+
+
+//Multiplicación
+function multiplicar(x, y){
+    if(typeof x === 'number' && typeof y === 'number'){
+        return x * y;
+    }else{
+        return "Ambos parámetros deben ser números"
+    }
+}
+
+multiplicar(4, 4);
+multiplicar("texto", "texto");
+
+
+//Invierno
+function esInvierno(mes){
+    if(mes == "junio" || mes == "julio" || mes == "agosto"){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+esInvierno("agosto");
+esInvierno("enero");
+
+
+//Covid (Profe en esta me ayude con chat gpt porque no sabia como hacerla, de igual forma entendi)
+const sintomasPacienteEnfermo = ["fiebre", "dolor muscular", "vómitos"];
+const sintomasPacienteNoEnfermo = ["fiebre", "titila ojo", "vómitos"];
+const sintomasCovid19 = ["fiebre", "dolor muscular", "pérdida de gusto"];
+
+function esCovid(sintomasPaciente, sintomasCovid){
+    let coincidencias = 0;
+    for(let i = 0; i < sintomasPaciente.length; i++){
+        if(sintomasCovid.includes(sintomasPaciente[i])){
+            coincidencias++;
+        }
+    }if(coincidencias >= 2){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+esCovid(sintomasPacienteEnfermo, sintomasCovid19);
+esCovid(sintomasPacienteNoEnfermo, sintomasCovid19);
+
+
+//Horario cursada (En esta tambien me ayude con chat gpt)
+function horarioCursada(materia){
+    const materias = {
+        "MATEMÁTICA": "JUEVES",
+        "LENGUA": "LUNES",
+        "GEOGRAFÍA": "VIERNES"
+    }
+    if(materias.has0wnProperty(materia.toUpperCase())){
+        return materias[materia.toUpperCase()];
+    }else{
+        return "Materia no encontrada";
+    }
+}
+
+horarioCursada("Matematica");
+horarioCursada("Física");
